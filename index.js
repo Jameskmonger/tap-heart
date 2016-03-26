11f3f9129de0e5a5c13e07b8095204f4a3dd6dea
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 23:47:42 +0000

    Print hearts and crosses

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 
 var through = require('through2');
 var parser = require('tap-parser');
+var figures = require('figures');
 
 var output = through();
 var input = parser();
 
 process.stdin.pipe(input);
 
+var getAssertionMessage = function (assert) {
+    if (assert.ok) {
+        return figures.heart;
+    }
+
+    return 'X';
+};
+
 input.on('assert', function (assert) {
-    output.push('an assertion: ' + assert.ok + '\n');
+    output.push(getAssertionMessage(assert));
 });
 
 output.pipe(process.stdout);

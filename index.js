c4da8bc999d1ab9d7fdc6a6b1d5e8da6497ccda8
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 23:17:09 +0000

    Listen to assertions

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var through = require('through2');
 var parser = require('tap-parser');
 
+var out = through();
 var p = parser();
 
 p.on('assert', function (assert) {
-    console.log(assert);
+    out.push('an assertion: ' + assert.ok + '\n');
 });
 
 process.stdin.pipe(p);
+out.pipe(process.stdout);
